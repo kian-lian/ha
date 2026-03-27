60706387b09a55f46f59f76a3898a700c092c970
lian <2088486180@qq.com>
2026-03-27 22:31:24 +0800

    refactor: rename all package names from @mono/* to @loom/*

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -4,8 +4,8 @@ import { Command } from "commander"
 import { createCommand } from "./commands/create.js"
 
 const program = new Command()
-  .name("mono-cli")
-  .description("Mono Workspace CLI tool")
+  .name("loom-cli")
+  .description("Loom Workspace CLI tool")
   .version("0.0.0", "-v, --version")
 
 program.addCommand(createCommand)
